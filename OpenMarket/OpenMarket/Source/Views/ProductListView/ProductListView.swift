9e0e1ac6affbb1f1ed5b5ed8084e9f53561556e7
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-03-20 20:20:48 +0900

    feat: ViewController -> ProductListView로 네이밍 변경

---
+++ OpenMarket/OpenMarket/Source/Views/ProductListView/ProductListView.swift
@@ -0,0 +1,17 @@
+//  OpenMarket - ProductListView.swift
+//  Created by zhilly on 2023/03/18
+
+import UIKit
+
+final class ProductListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .white
+    }
+}
