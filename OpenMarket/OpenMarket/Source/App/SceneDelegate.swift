51abed3d01ca0fbc830a781e02dffabe85c1acf0
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-03-20 20:19:39 +0900

    feat: TabBarController 구현

--- OpenMarket/OpenMarket/Source/App/SceneDelegate.swift
+++ OpenMarket/OpenMarket/Source/App/SceneDelegate.swift
@@ -3,7 +3,7 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
@@ -13,10 +13,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         options connectionOptions: UIScene.ConnectionOptions
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let viewController = ViewController()
+        let tabBarController = UITabBarController()
+        let productListViewController = UINavigationController(rootViewController: ProductListViewController())
+        let searchViewController = UINavigationController(rootViewController: SearchViewController())
+        
+        tabBarController.setViewControllers([productListViewController,
+                                             searchViewController], animated: true)
+        
+        if let items = tabBarController.tabBar.items {
+            items[safe: 0]?.selectedImage = UIImage(systemName: "house.fill")
+            items[safe: 0]?.image = UIImage(systemName: "house")
+            items[safe: 0]?.title = "목록"
+            
+            items[safe: 1]?.selectedImage = UIImage(systemName: "magnifyingglass.circle.fill")
+            items[safe: 1]?.image = UIImage(systemName: "magnifyingglass.circle")
+            items[safe: 1]?.title = "검색"
+        }
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = viewController
+        window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
     }
 }
