3be6b1925ac5d3d5a3343c9dddbe0576c51f5c60
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-03-20 19:09:26 +0900

    feat: API에 사용하는 Constant 작성

---
+++ OpenMarket/OpenMarket/Source/Network/API/APIConstant.swift
@@ -0,0 +1,22 @@
+//  OpenMarket - APIConstant.swift
+//  Created by zhilly on 2023/03/18
+
+import Foundation
+
+struct APIConstant {
+    static var baseURL: URL {
+        return URL(string: "https://openmarket.yagom-academy.kr")!
+    }
+    static let venderID = "zhilly"
+    static let secret = "rzeyxdwzmjynnj3f"
+    static let identifier = "f44cfc3e-6941-11ed-a917-47bc2e8f559b"
+}
+
+enum HTTPHeaderField: String {
+    case identifier = "identifier"
+    case contentType = "Content-Type"
+}
+
+enum ContentType: String {
+    case json = "application/json"
+}
