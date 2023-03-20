37ac824b40c9bd3e0e2327881219514f5f5b9cdf
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-03-20 20:19:13 +0900

    feat: Array를 안전하게 조회할 수 있도록 추가기능 구현

---
+++ OpenMarket/OpenMarket/Source/Extensions/Array+Extension.swift
@@ -0,0 +1,8 @@
+//  OpenMarket - Array+Extension.swift
+//  Created by zhilly on 2023/03/18
+
+extension Array {
+    public subscript (safe index: Int) -> Element? {
+        return indices ~= index ? self[index] : nil
+    }
+}
