7392c64e5f18db10b0355a0e5219facc0cf05045
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-03-20 19:37:13 +0900

    feat: post, patch, delete하는 메서드 기본 구현

--- OpenMarket/OpenMarket/Source/Network/NetworkManager.swift
+++ OpenMarket/OpenMarket/Source/Network/NetworkManager.swift
@@ -9,9 +9,9 @@ final class NetworkManager {
     typealias onFailure = ((_ error: Error) -> Void)
     
     private func request<T>(_ object: T.Type,
-                    router: APIRouter,
-                    success: @escaping onSuccess<T>,
-                    failure: @escaping onFailure) where T: Decodable {
+                            router: APIRouter,
+                            success: @escaping onSuccess<T>,
+                            failure: @escaping onFailure) where T: Decodable {
         AF.request(router)
             .validate(statusCode: 200..<300)
             .response { data in
@@ -63,8 +63,7 @@ extension NetworkManager: OpenMarketAPI {
     func inquiryProductList(pageNumber: Int,
                             itemsPerPage: Int,
                             searchValue: String? = nil,
-                            completion: @escaping (Result<ProductList, OpenMarketAPIError>) -> Void
-    ) {
+                            completion: @escaping (Result<ProductList, OpenMarketAPIError>) -> Void) {
         request(ProductList.self,
                 router: .inquiryProductList(pageNumber: pageNumber,
                                             itemsPerPage: itemsPerPage,
@@ -73,5 +72,26 @@ extension NetworkManager: OpenMarketAPI {
         } failure: { error in
             completion(.failure(.unknownError))
         }
+        
+    }
+    
+    func createProduct() {}
+    
+    func inquiryDeleteURI(id: Int) {
+        request(String.self,
+                router: .inquiryDeleteURI(id: id)) { item in
+            print("delete ULI = \(item)")
+            debugPrint(item)
+        } failure: { error in
+            print(error.localizedDescription)
+        }
+    }
+    
+    func updateProduct(id: Int) {
+        
+    }
+    
+    func deleteProduct(path: String) {
+        
     }
 }
