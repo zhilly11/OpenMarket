c18ba3e3f0de05f621aaf5a66d167107c6d44506
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-03-26 16:29:10 +0900

    feat: String에 Stroke 해주는 기능 구현

---
+++ OpenMarket/OpenMarket/Source/Utils/String+Extension.swift
@@ -0,0 +1,18 @@
+//  OpenMarket - String+Extension.swift
+//  Created by zhilly on 2023/03/23
+
+import UIKit
+
+extension String {
+    func strikeThrough(length: Int, color: UIColor) -> NSAttributedString {
+        let attributeString = NSMutableAttributedString(string: self)
+
+        attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle,
+                                     value: NSUnderlineStyle.single.rawValue,
+                                     range: NSMakeRange(0, length))
+        attributeString.addAttributes([NSAttributedString.Key.foregroundColor : color],
+                                      range: NSMakeRange(0, length))
+
+        return attributeString
+    }
+}
