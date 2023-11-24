5bb292775dbce907146dfbef6f8a739532152f0d
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-11-24 18:23:42 +0900

    ⚰️ Remove Dead Code

--- OpenMarket/OpenMarket/Source/Pages/SearchView/SearchViewModel.swift
+++ OpenMarket/OpenMarket/Source/Pages/SearchView/SearchViewModel.swift
@@ -10,21 +10,6 @@ final class SearchViewModel {
     private let disposeBag = DisposeBag()
     
     func search(_ searchValue: String) {
-        if searchValue == "" {
-            return
-        } else {
-            _ = APIService.inquiryProductList(pageNumber: 1, itemsPerPage: 30, searchValue: searchValue)
-                .map { items -> [Product] in
-                    return items.pages
-                }
-                .take(1)
-                .subscribe(
-                    onNext: { self.productList.onNext($0) },
-                    onError: {
-                        self.productList.onNext([])
-                        print($0.localizedDescription)
-                    }
-                )
-        }
+        
     }
 }
