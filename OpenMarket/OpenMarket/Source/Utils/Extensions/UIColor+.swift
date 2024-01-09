d957766646fcff376851464d5336b60615ca4af2
Zhilly <99257965+zhilly11@users.noreply.github.com>
2024-01-09 15:06:56 +0900

    🍱 Add color assets

--- OpenMarket/OpenMarket/Source/Utils/Extensions/UIColor+.swift
+++ OpenMarket/OpenMarket/Source/Utils/Extensions/UIColor+.swift
@@ -4,12 +4,20 @@
 import UIKit.UIColor
 
 extension UIColor {
+    static var main: UIColor {
+        return UIColor(named: "MainColor") ?? UIColor.orange
+    }
+    
     static var customGray: UIColor {
         return UIColor(named: "CustomBackground") ?? UIColor.systemGray4
     }
     
     static var customBackground: UIColor {
         return color(light: .white, dark: .customGray)
+    }
+    
+    static var separator: UIColor {
+        return UIColor(named: "Separator") ?? UIColor.systemGray4
     }
     
     private static func color(light: UIColor, dark: UIColor) -> UIColor {
