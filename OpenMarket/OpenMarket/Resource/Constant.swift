bbcf810ebe3b28b288530cae8c5aa0de79630c87
Zhilly <99257965+zhilly11@users.noreply.github.com>
2024-01-18 01:16:20 +0900

    📝 Add App Constant

---
+++ OpenMarket/OpenMarket/Resource/Constant.swift
@@ -0,0 +1,13 @@
+//  OpenMarket - Constant.swift
+//  Created by zhilly on 2024/01/16
+
+enum Constant {
+    struct Placeholder {
+        static let description: String = """
+        오픈마켓에 올릴 게시글 내용을 작성해주세요.
+        (판매 금지 물품은 게시가 제한될 수 있어요.)
+        
+        신뢰할 수 있는 거래를 위해 자세히 적어주세요.
+        """
+    }
+}
